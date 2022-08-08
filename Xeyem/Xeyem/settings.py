2a51bb45142c45cf9af17b392a99c0504bd8973d
nborregan <ignacio.borregan@udc.es>
2022-08-09 01:03:27 +0200

    hechas primeras migraciones y modificado settings.py

--- Xeyem/Xeyem/settings.py
+++ Xeyem/Xeyem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -80,6 +81,8 @@ DATABASES = {
     }
 }
 
+
+AUTH_USER_MODEL = 'app.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
