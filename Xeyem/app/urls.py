9b07c352e39f1f0b7bb425e8c2c056c2a51cd1cd
nborregan <ignacio.borregan@udc.es>
2022-08-09 11:16:40 +0200

    Implementada vista de lista de dashboards

--- Xeyem/app/urls.py
+++ Xeyem/app/urls.py
@@ -1,10 +1,10 @@
 from django import urls
 from django.urls import path
-from .views import IndexView, CustomLoginView, RegisterPage
+from .views import DashboardList, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', DashboardList.as_view(), name='index'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
--- Xeyem/app/views.py
+++ Xeyem/app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Dashboard
 from .forms import UserCreationForm
 
 from django.contrib.auth import login
@@ -37,8 +41,26 @@ class RegisterPage(FormView):
 
 
 # Dashboard Views
+class DashboardList(ListView):
+    model = Dashboard
+    context_object_name = 'dashboards'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['dashboards'] = context['dashboards'].filter(user_id=self.request.user)
+            context['count'] = context['dashboards'].count()
+            
+            search_input = self.request.GET.get('search-area') or ''
+            if search_input:
+                context['dashboards'] = context['dashboards'].filter(
+                    title__contains=search_input)
+            context['search_input'] = search_input
+            
+            context['default_dashboard'] = context['dashboards'].filter(default_dashboard=True)
+
+        return context
 
 
-
-class IndexView(TemplateView):
-    template_name = 'app/index.html'
+# class IndexView(TemplateView):
+#     template_name = 'app/index.html'
