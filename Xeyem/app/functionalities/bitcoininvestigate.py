d705f2bf77d5f1715a04b32ddab9a1470d46c8d5
nborregan <ignacio.borregan@udc.es>
2022-08-21 21:50:51 +0200

    Implementación de obtención de fecha corregida

--- Xeyem/app/functionalities/bitcoininvestigate.py
+++ Xeyem/app/functionalities/bitcoininvestigate.py
@@ -1,4 +1,7 @@
 from requests import get
+from math import ceil
+import pandas as pd
+import ssl
 
 SATOSHI_DIVISOR = 100000000
 
@@ -15,16 +18,42 @@ def __blockchain_info_parser(json_response: dict, address: str) -> dict:
     json_response['txs'] = sorted(aux_list, key=lambda d: d['time'])
     return  json_response
 
+def __get_first_and_last_tx(address: str, n_tx: int) -> tuple:
+    url = f'https://www.walletexplorer.com/address/{address}'
+    # To avoid certificate errors
+    ssl._create_default_https_context = ssl._create_unverified_context
+    
+    # We use pandas to get the LAST transaction
+    data = pd.read_csv(url+'?format=csv')
+    data = data.reset_index() # we reset the index
+    # And finally we return the first row from the date column
+    last_tx = f'{data.level_0.iloc[1]}'
+    
+    # The first transaction is at the last row on the last page
+    # the amount of txs for page is 100
+    if n_tx <= 100: # if there is only 1 page we already have it
+        first_tx = f'{data.level_0.iloc[-1]}'
+    else:
+        page_n = ceil(n_tx / 100)
+        url += f'?page={page_n}'
+
+        data = pd.read_csv(url+'&format=csv')
+        data = data.reset_index()
+
+        first_tx = f'{data.level_0.iloc[-1]}'
+        
+    return (first_tx,last_tx)
+
 def get_common_info(address: str) -> dict:
     res = get(f'https://blockchain.info/rawaddr/{address}')
     if res.ok:
         json_response = res.json()
         txs_n = len(json_response['txs'])
-        if txs_n == 100:
-            res2 = get(f'https://blockchain.info/rawaddr/{address}?offset=100')
-            if res2.ok:
-                json_response['txs'] += res2.json()['txs']
-                txs_n = len(res2.json()['txs'])
+        # if txs_n == 100:
+        #     res2 = get(f'https://blockchain.info/rawaddr/{address}?offset=100')
+        #     if res2.ok:
+        #         json_response['txs'] += res2.json()['txs']
+        #         txs_n = len(res2.json()['txs'])
 
         return __blockchain_info_parser(json_response, address)
 
@@ -35,7 +64,12 @@ def balance(info: dict) -> dict:
     }
     
 def fst_lst_transaction(info: dict) -> dict:
+    if info is not None:
+        first_transaction,last_transaction = __get_first_and_last_tx(info['address'], info['n_tx'])
+    else:
+        first_transaction = last_transaction = "Error al obtener fecha"
+        
     return {
-        'first_transaction': info['txs'][0]['time'],
-        'last_transaction': info['txs'][-1]['time'],
+        'first_transaction': first_transaction,
+        'last_transaction': last_transaction,
     }
