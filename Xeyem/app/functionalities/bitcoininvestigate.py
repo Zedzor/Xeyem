aa286b45a9a442e80432b72d78e0be969dd31ffc
nborregan <ignacio.borregan@udc.es>
2022-09-05 02:45:09 +0200

    Terminada lógica de casos de uso illegal_activity y transactions

--- Xeyem/app/functionalities/bitcoininvestigate.py
+++ Xeyem/app/functionalities/bitcoininvestigate.py
@@ -7,21 +7,14 @@ from bs4 import BeautifulSoup
 import re
 import plotly.express as px
 from fp.fp import FreeProxy
+from datetime import datetime
 
 
+BITCOINABUSE_API_TOKEN = 'xpq3nxlgXOcyafM6QaBCXCu3oKjArNT9Q2bZfzIX'
 SATOSHI_DIVISOR = 100000000
 
 def __blockchain_info_parser(json_response: dict, address: str) -> dict:
     aux_list = []
-    # Parsing transaction list
-    for tx in json_response['txs']:
-        is_in_tx = False
-        # Detecting if the address is in the input or output
-        for entry in tx['inputs']:
-            if entry['prev_out']['addr'] == address:
-                is_in_tx = True
-        tx['is_in_tx'] = is_in_tx
-        aux_list.append(tx)
     # We sort the list by time
     json_response['txs'] = sorted(aux_list, key=lambda d: d['time'])
     return  json_response
@@ -63,6 +56,60 @@ def __get_different_proxy(proxies: list) -> str:
         proxy = FreeProxy(rand=True).get()
     return proxy
 
+def __get_wallets_from_walletexplorer():
+    url = 'https://www.walletexplorer.com'
+    proxy = __get_different_proxy([])
+    res = get(url=url, proxies={'http': proxy})
+    if res.ok:
+        soup = BeautifulSoup(res.text, 'html.parser')
+        table = soup.find('table', {'class': 'serviceslist'})
+        table = table.tr 
+        wallets = {}
+        for services in table.children:
+            service_type = re.sub(r'/\w+$', '', services.h3.string.replace(':', '').lower())
+            for service in services.ul.children:
+                if service.name == 'li':
+                    urls = [url + a['href'] for a in service.find_all('a')]
+                    wallets[service.a.string] = {
+                        'type': service_type,
+                        'urls': urls
+                    }
+        return wallets 
+    else:
+        return None
+
+def __get_label_from_address(address: str) -> str:
+    res = get(f'https://www.walletexplorer.com/address/{address}')
+    if res.ok:
+        soup = BeautifulSoup(res.text, 'html.parser')
+        div = soup.find('div', {'class': 'walletnote'})
+        wallet =  div.find('a')['href'].split('/')[-1]
+        return wallet
+    else:
+        return None
+
+def __get_report_from_bitcoinabuse(address: str) -> str:
+    abuse = None
+    res = get(f'https://www.bitcoinabuse.com/api/reports/check?address={address}&api_token={BITCOINABUSE_API_TOKEN}')
+    if res.ok:
+        if res.json()['count'] > 0:
+            abuse_types = get('https://www.bitcoinabuse.com/api/abuse-types').json()
+            abuse_types = {abuse_type['id']: abuse_type['label'] for abuse_type in abuse_types}
+            abuse_reports = res.json()['recent']
+            # count the number of reports for each abuse type and return the most common
+            for report in abuse_reports:
+                report['abuse_type_label'] = abuse_types[report['abuse_type_id']]
+            abuse_reports = pd.DataFrame(abuse_reports)
+            abuse_reports = abuse_reports.groupby('abuse_type_id').count().sort_values(by=['abuse_type_label','abuse_type_id'], ascending=[False,True])
+            abuse_reports = abuse_reports.reset_index()
+            abuse_reports = abuse_reports.iloc[0]
+            abuse = abuse_types[abuse_reports['abuse_type_id']]
+    
+    if abuse is None:
+        abuse = "No se ha encontrado actividad ilegal"
+    
+    return abuse
+
 def get_common_info(address: str) -> dict:
     res = get(f'https://blockchain.info/rawaddr/{address}')
     if res.ok:
@@ -71,7 +118,7 @@ def get_common_info(address: str) -> dict:
         total_req = ceil(json_response['n_tx'] / 100)
         # Number of requests to be executed (max 10 == 1000 txs)
         req_n = total_req if total_req < 10 else 10
-        for i in range(req_n - 1):
+        for i in range(1, req_n):
             res_ok = False
             proxy_list = []
             failed_count = 0
@@ -134,3 +181,58 @@ def balance_time(address: str) -> dict:
         plt_div = "Couldn't get balance through time"
     
     return {'balance_time': plt_div}
+
+def transactions(info: dict) -> dict:
+    if info is not None:
+        txs = info['txs']
+        transactions = []
+        for tx in txs:
+            inputs = []
+            for inp in tx['inputs']:
+                inputs.append({
+                    'address': inp['prev_out']['addr'],
+                    'value': inp['prev_out']['value'] / SATOSHI_DIVISOR,
+                })
+            outputs = []
+            for out in tx['out']:
+                outputs.append({
+                    'address': out['addr'],
+                    'value': out['value'] / SATOSHI_DIVISOR,
+                })
+            custom_tx = {
+                'hash' : tx['hash'],
+                'date' : datetime.fromtimestamp(tx['time']),
+                'inputs': inputs,
+                'outputs': outputs,
+                'is_input': tx['result'] < 0,
+                'value': tx['result'] / SATOSHI_DIVISOR,
+            }
+            transactions.append(custom_tx)       
+    else:
+        transactions = 'Error al obtener transacciones'
+        
+    return {
+        'transactions': transactions,
+    }
+
+
+def illegal_activity(address: str) -> dict:
+    illegal_activity = None
+    label = __get_label_from_address(address)
+    # Exclude addresses with label 'Exchange' or 'Pool'
+    if label is not None:
+        wallets = __get_wallets_from_walletexplorer()
+        if wallets is not None:
+            for key in wallets:
+                if key in label:
+                    if wallets[key]['type'] in ['exchanges', 'pools']:
+                        illegal_activity = "No se ha encontrado actividad ilegal"
+                        break
+                    else:
+                        illegal_activity = wallets[key]['type']
+                        break
+    if illegal_activity is None:
+        illegal_activity =__get_report_from_bitcoinabuse(address)
+    return {
+        'illegal_activity': illegal_activity,
+    }
