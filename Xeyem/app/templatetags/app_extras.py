35634ca2ea25d908a69b5258896c69e1c072a14f
Zedzor <nborregan@protonmail.com>
2022-10-25 23:59:22 -0400

    Implemented filters

--- Xeyem/app/templatetags/app_extras.py
+++ Xeyem/app/templatetags/app_extras.py
@@ -5,3 +5,20 @@ register = template.Library()
 @register.filter
 def modulo(num, val):
     return num % val
+
+@register.filter
+def separate(value: str):
+    return value.split()
+
+@register.filter
+def total_votes(upvotes: str, downvotes: str):
+    uv = upvotes.split(",") if upvotes != "" else []
+    dv = downvotes.split(",") if downvotes != "" else []
+    # not count '' as vote
+    for i in uv:
+        if i == '':
+            uv.remove(i)
+    for i in dv:
+        if i == '':
+            dv.remove(i)
+    return len(uv) - len(dv)
